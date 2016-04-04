562714622346a49f19c6cc9976e90549e7c31cc8
Sergio <Sergio@SergioPC>
2016-04-04 16:03:54 +0200

    Versió 0.1 del projecte pr02_m8

---
+++ src/pr02_m8/Pr02_m8.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pr02_m8;
+
+/**
+ *
+ * @author Sergio
+ */
+public class Pr02_m8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+       public static void main(String[] args) { 
+System.out.println("Hola món"); 
+System.out.println("Versió 0.1 del projecte pr02"); 
+} 
+    
+    
+}
